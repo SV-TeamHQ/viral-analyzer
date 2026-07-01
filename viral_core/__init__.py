d18c66554151ffe82227124e49c17332d003ae5c
shika-stuck <shikakage@gmail.com>
2026-07-01 10:48:01 +0800

    feat: add viral_core.paths (durable run directories)

--- viral_core/__init__.py
+++ viral_core/__init__.py
@@ -1,4 +1,5 @@
 from viral_core.scoring import outlier_score
 from viral_core.config_io import load_competitors, save_competitors, load_env
+from viral_core.paths import new_run_dir, latest_run, run_artifact
 
-__all__ = ["outlier_score", "load_competitors", "save_competitors", "load_env"]
+__all__ = ["outlier_score", "load_competitors", "save_competitors", "load_env", "new_run_dir", "latest_run", "run_artifact"]
