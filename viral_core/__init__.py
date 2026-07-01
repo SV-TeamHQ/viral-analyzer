0845640934cb61f3c1ecaedc445cebf61d89aabb
shika-stuck <shikakage@gmail.com>
2026-07-01 10:53:52 +0800

    feat: add viral_core.apify_client.run_actor

--- viral_core/__init__.py
+++ viral_core/__init__.py
@@ -1,5 +1,6 @@
 from viral_core.scoring import outlier_score
 from viral_core.config_io import load_competitors, save_competitors, load_env
 from viral_core.paths import new_run_dir, latest_run, run_artifact
+from viral_core.apify_client import run_actor
 
-__all__ = ["outlier_score", "load_competitors", "save_competitors", "load_env", "new_run_dir", "latest_run", "run_artifact"]
+__all__ = ["outlier_score", "load_competitors", "save_competitors", "load_env", "new_run_dir", "latest_run", "run_artifact", "run_actor"]
